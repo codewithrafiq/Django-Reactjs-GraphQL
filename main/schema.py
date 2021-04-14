431778a56b634194004fdd9199bf752bb4dd7307
rafiquljislam <rafiquljislam@gmail.com>
2021-04-14 19:45:28 +0600

    part-2

--- main/schema.py
+++ main/schema.py
@@ -6,4 +6,8 @@ class Query(schema.Query, graphene.ObjectType):
     pass
 
 
-schema = graphene.Schema(query=Query)
+class Mutation(schema.Mutation, graphene.ObjectType):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
--- todo/schema.py
+++ todo/schema.py
@@ -10,7 +10,53 @@ class TodoType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    todos = DjangoListField(TodoType)
+    todos = graphene.List(TodoType, id=graphene.Int())
 
-    def resolve_todos(self, info):
+    def resolve_todos(self, info, id=None):
+        if id:
+            return Todo.objects.filter(id=id)
         return Todo.objects.all()
+
+
+class CreateTodo(graphene.Mutation):
+    todo = graphene.Field(TodoType)
+
+    class Arguments:
+        title = graphene.String(required=True)
+
+    def mutate(self, info, title):
+        todo = Todo(title=title)
+        todo.save()
+        return CreateTodo(todo=todo)
+
+
+class UpdateTodo(graphene.Mutation):
+    todo = graphene.Field(TodoType)
+
+    class Arguments:
+        id = graphene.Int(required=True)
+        title = graphene.String(required=True)
+
+    def mutate(self, info, id, title):
+        todo = Todo.objects.get(id=id)
+        todo.title = title
+        todo.save()
+        return UpdateTodo(todo=todo)
+
+
+class DelateTodo(graphene.Mutation):
+    message = graphene.String()
+
+    class Arguments:
+        id = graphene.Int(required=True)
+
+    def mutate(self, info, id):
+        todo = Todo.objects.get(id=id)
+        todo.delete()
+        return DelateTodo(message=f"ID {id} id Delated")
+
+
+class Mutation(graphene.ObjectType):
+    create_todo = CreateTodo.Field()
+    update_todo = UpdateTodo.Field()
+    delate_todo = DelateTodo.Field()
